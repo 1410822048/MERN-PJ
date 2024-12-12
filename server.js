43790d49cfabc5728a90ea0e76ce934bba754343
Jackie Zheng <jackie26811859@gmail.com>
2024-12-12 17:23:14 +0800

    I dont know

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-// const express = require("express");
+const express = require("express");
 const path = require("path");
-const express = require(path.join(__dirname, "node_modules", "express"));
 const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
